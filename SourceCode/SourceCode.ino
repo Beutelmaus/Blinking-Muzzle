09526048e745709449fac2191a81f9e4af4db6cd
Beutelmaus <144056060+Beutelmaus@users.noreply.github.com>
2025-06-15 18:46:20 +0200

    implement onboard-LED
    set the onboard led to the value of the first led in the string to be able to see the animation from the inside

--- SourceCode/SourceCode.ino
+++ SourceCode/SourceCode.ino
@@ -2,16 +2,18 @@
  *
 * RESOURCES:
 * NeoPixel Uberguide: https://learn.adafruit.com/adafruit-neopixel-uberguide
+* ScorpioRP2040_Pinout: https://learn.adafruit.com/introducing-feather-rp2040-scorpio/pinouts
 * NeoPXL8 Strandtest:https://github.com/adafruit/Adafruit_NeoPXL8/blob/master/examples/NeoPXL8/strandtest/strandtest.ino
 */
 
-
-#include <Adafruit_NeoPXL8.h>
+#include <Adafruit_NeoPXL8.h>//Strips
+#include <Adafruit_NeoPixel.h>//Onboard_LED
+Adafruit_NeoPixel OnboardLED(1, 4, NEO_GRB + NEO_KHZ800);
 
 #define NUM_LEDS    9      // NeoPixels PER STRAND, total number is 8X this!
+int8_t pins[8] = {16, 17, 18, 19, 20, 21, 22, 23 };
+Adafruit_NeoPXL8 leds(NUM_LEDS, pins, NEO_GRB + NEO_KHZ800);
 
-// For the Feather RP2040 SCORPIO, use this list:
-int8_t pins[8] = { 16, 17, 18, 19, 20, 21, 22, 23 };
 
 #define BoopSensorIn 6
 #define BoopSensorOut 5
@@ -21,15 +23,19 @@ int8_t pins[8] = { 16, 17, 18, 19, 20, 21, 22, 23 };
 
 #define Number_OF_Programms 6
 
-Adafruit_NeoPXL8 leds(NUM_LEDS, pins, NEO_GRB + NEO_KHZ800);
 
 
 
-uint16_t currentProgram, currentBrightness = 1;
+uint16_t currentProgram = 1;
 const char* programNames[] = {"", "Off","Rainbow","Magenta","Green","Cyan","Red"};// Names for display in Serial Monitor
 uint16_t ProgramLastCycle = 1;
 bool lastButtonState = LOW;
 
+unsigned long lastDebounceTime = 0;
+bool Brightness_lastState = LOW;
+uint16_t Brighness_Level = 1;
+byte Brightness_Selected  = 250;
+byte Brightness_LastCycle  = 250;
 
 uint16_t i, j;//Used in For loops
 
@@ -53,14 +59,16 @@ void setup() {
   pinMode(BrightnesSensorOut, OUTPUT);
   digitalWrite(BrightnesSensorOut, 1);
   
-  
   Serial.begin(9600);
 
   if (!leds.begin()) {// Blink the onboard LED if something was configurn wrong.
     pinMode(LED_BUILTIN, OUTPUT);
-    for (;;) digitalWrite(LED_BUILTIN, (millis() / 500) & 1);
+    for (;;){
+      digitalWrite(LED_BUILTIN, (millis() / 1000) & 1);
+      Serial.println("Configuration Error");
+    }
   }
-  leds.setBrightness(250);
+  leds.setBrightness(10);
   
   for (uint32_t color = 0xFF0000; color > 0; color >>= 8) {// Cycle all pixels red/green/blue on startup. 
     leds.fill(color);
@@ -69,6 +77,10 @@ void setup() {
     digitalWrite(BoopSensorOut, 1);//Sensor teaches value when switching on
   }
   currentProgram = 1;//Start with all LED's off
+
+  OnboardLED.begin();
+  OnboardLED.setBrightness(10);
+  OnboardLED.show();
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop() {
@@ -77,8 +89,11 @@ void loop() {
       leds.clear();
       leds.show();
       CheckBoopSensor();
+
+      OnboardLED.clear();
+      OnboardLED.show();
       break;
-    case 2: RainbowCycle(5200);break;//Pass cycle time in ms
+    case 2: rainbow(50);break;//Pass cycle time in ms
     case 3: WavePulseAllLeds(colors[7], 40,  750);break;//Magenta, 40%, 0,75s
     case 4: WavePulseAllLeds(colors[3], 10, 3000);break;//Green
     case 5: WavePulseAllLeds(colors[4], 80, 5000);break;//Cyan
@@ -87,13 +102,14 @@ void loop() {
   }
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 void CheckBoopSensor() {
   bool currentState = digitalRead(BoopSensorIn);
-  bool Brightness_currentState = digitalRead(BrightnesSensorIn);
+  ProgramLastCycle  = currentProgram;
  
   //Debug Output
   Serial.print("Button: ");      Serial.print(currentState == HIGH ? "Off" : "On");
-  Serial.print(" | Brightness: ");  Serial.print(currentBrightness);
+  CheckBrightnessSensor();  Serial.print(" | Brightness: ");  Serial.print(Brighness_Level);
   Serial.print(" | Program: ");  Serial.print(currentProgram);
   Serial.print(" - ");           Serial.println(programNames[currentProgram]);
 
@@ -103,24 +119,25 @@ void CheckBoopSensor() {
     }
 
   lastButtonState = currentState;
-
- if (Brightness_currentState == HIGH) {
-          currentBrightness++;
-     if (currentBrightness > 7) currentBrightness = 1;
-      switch (currentBrightness) {
-        case 1: leds.setBrightness(250); leds.show(); break;
-        case 2: leds.setBrightness(200); leds.show(); break;
-        case 3: leds.setBrightness(150); leds.show(); break;
-        case 4: leds.setBrightness(100); leds.show(); break;
-        case 5: leds.setBrightness(50);  leds.show(); break;
-        case 6: leds.setBrightness(15);  leds.show(); break;
-        case 7: leds.setBrightness(5);   leds.show(); break;
-}
-      delay(500);
-    }
+  CheckBrightnessSensor();
+  SetOnboardLed();
 }
 
+void CheckBrightnessSensor() {
+  bool Brightness_currentState = digitalRead(BrightnesSensorIn);
 
+    if (Brightness_currentState == HIGH && Brightness_lastState == LOW) {
+      Brighness_Level++;
+      if (Brighness_Level > 4) Brighness_Level = 1;
+  }
+
+  Brightness_lastState = Brightness_currentState;
+}
+void SetOnboardLed() {
+  uint32_t color = leds.getPixelColor(0); // First pixel of first strip
+  OnboardLED.setPixelColor(0, color);
+  OnboardLED.show();
+}
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 // Single Colour
 
@@ -132,7 +149,6 @@ void SetAllLedsTo(uint8_t color[3]) {
     leds.setPixelColor(i + NUM_LEDS, _color);// Strip 2
     // fill command would do the same but this is function is usefull for testing
   }
-
   leds.show();
   CheckBoopSensor();
 }
@@ -204,37 +220,16 @@ void WavePulseAllLeds(uint8_t color[3], uint8_t startPercent, uint16_t cycleTime
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 // Rainbow Cycle
-void RainbowCycle(int cycleTime) {
-  byte* c;
-uint16_t currentPixel;
-  for (j = 0; j < 256; j++) {
-    for (i = 0; i < NUM_LEDS; i++) {
-      c = RainbowWheel(((i * 256 / NUM_LEDS) + j) & 255);
-      currentPixel = (NUM_LEDS - 1 - i);
-      leds.setPixelColor(currentPixel           , leds.Color(*c, *(c + 1), *(c + 2)));//Strip 1
-      leds.setPixelColor(currentPixel + NUM_LEDS, leds.Color(*c, *(c + 1), *(c + 2)));//Strip 2
-   }
+ void rainbow(int wait) {
+  for(long firstPixelHue = 0; firstPixelHue < 3*65536; firstPixelHue += 256) {
+    for(int i=0; i<NUM_LEDS; i++) {
+      int pixelHue = firstPixelHue + (i * 65536L / NUM_LEDS);
+      leds.setPixelColor(i           , leds.gamma32(leds.ColorHSV(pixelHue)));//Strip 1
+      leds.setPixelColor(i + NUM_LEDS, leds.gamma32(leds.ColorHSV(pixelHue)));//Strip 2
+    }
     leds.show();
     CheckBoopSensor();
-    if (currentProgram != ProgramLastCycle) return; // Exit immediately if program changed
-    delay(cycleTime/256);
-    }
-    Serial.println(" Finished Rainbow Cycle");}
-
-byte* RainbowWheel(byte WheelPosition) {
-  static byte c[3];
-  if (WheelPosition < 85) {
-    c[0] = WheelPosition * 3;
-    c[1] = 255 - WheelPosition * 3;
-    c[2] = 0;
-  } else if (WheelPosition < 170) {
-    WheelPosition -= 85;
-    c[0] = 255 - WheelPosition * 3;
-    c[1] = 0;
-    c[2] = WheelPosition * 3;
-  } else {
-    WheelPosition -= 170;
-    c[0] = 0;
-    c[1] = WheelPosition * 3;
-    c[2] = 255 - WheelPosition * 3;  }
-  return c;}
+    if (currentProgram != 2) return;
+    delay(wait); 
+  }
+}
